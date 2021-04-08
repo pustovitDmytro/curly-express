e721f00b1c5f11bea32ada63f17ce9e039676bba
Dmytro Pustovit <dipustovit@gmail.com>
2021-04-09 01:14:26 +0300

    New: adds urlencoded and basic auth

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,14 @@
-export function build({ method = 'GET', url, headers = {}, json, urlencoded, form, body }) {
+export function build({ method = 'GET', url, headers = {}, json, urlencoded, form, user }) {
     const command = [ 'curl' ];
 
     command.push('-X', method);
     Object.entries(headers).forEach(([ key, value ]) => {
         command.push('-H', `'${key}: ${value}'`);
     });
-    if (url.username) {
-        command.push('--user', `${url.username}:${url.password}`);
-    }
+
+    // if (url.username) command.push('--user', `${url.username}:${url.password}`);
+    if (user) command.push('--user', `${user.name}:${user.pass}`);
+
     if (json) {
         command.push('-d', `'${JSON.stringify(json)}'`);
     }
@@ -17,13 +18,11 @@ export function build({ method = 'GET', url, headers = {}, json, urlencoded, for
         });
     }
     if (form) {
-        Object.entries(form).forEach(([ key, value ]) => {
+        form.forEach(({ key, value }) => {
             command.push('-F', `'${key}=${value}'`);
         });
     }
-    if (body) {
-        command.push('-d', `'${body}'`);
-    }
+
     command.push(url.href);
 
     return command.join(' ');
