2f1dad9761c681f8cf6e0ab7083242b01f0c5e64
Dmytro Pustovit <dipustovit@gmail.com>
2021-04-09 01:14:40 +0300

    New: adds busboy

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
 import { URL } from 'url';
+import basicAuth from 'basic-auth';
 import { build } from './utils';
 
 export function cURL({ log, attach }) {
-    return function (req, res, next) {
+    return async function (req, res, next) {
+        const user  = basicAuth(req);
         const obj = {
             url     : new URL(`${req.protocol}://${req.get('host')}${req.originalUrl}`),
             method  : req.method,
-            headers : req.headers
+            headers : req.headers,
+            user
         };
 
-        if (req._body) {
-            obj.json =  req.body;
+        if (req.busboy) {
+            obj.form = [];
+            await new Promise(async (resolve) => {
+                req.busboy.on('file', function (fieldname, file, filename) {
+                    obj.form.push({ key: fieldname, value: `@${filename}` });
+                    file.on('data', () => {});
+                });
+                req.busboy.on('field', function (key, value) {
+                    obj.form.push({ key, value });
+                });
+                req.busboy.on('finish', resolve);
+
+                req.pipe(req.busboy);
+            });
+        }
+        let isJSON = req._body;
+
+        if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+            isJSON = false;
+            obj.urlencoded = req.body;
         }
 
+        if (isJSON) obj.json =  req.body;
         const curl = build(obj);
 
-        if (log) {
-            log(curl);
-        }
+        if (log) log(curl);
+
         if (attach) {
             const attachKey = attach === true ? '_curl' : attach;
 
