6d37bc4871c11f62a3d0f171d2882f26b10fd69b
Dmytro Pustovit <dipustovit@gmail.com>
2021-04-09 01:12:55 +0300

    Chore: adds tests to improve coverage

--- tests/package/express.test.js
+++ tests/package/express.test.js
@@ -1,8 +1,12 @@
 /* eslint-disable more/no-then */
+import fs from 'fs';
+import path from 'path';
 import jsonServer from 'json-server';
 import { assert } from 'chai';
 import axios from 'axios';
 import FormData from 'form-data';
+import busboy from  'connect-busboy';
+// import multiparty from 'connect-multiparty';
 import { cURL } from '../entry';
 
 const bodyParser = require('body-parser');
@@ -25,12 +29,18 @@ const requests = [];
 suite('Express');
 
 before(async () => {
+    const middleware = cURL({
+        log    : curl => requests.push(curl),
+        attach : true
+    });
+
     server.use(bodyParser.json());
     server.use(bodyParser.urlencoded({ extended: true }));
-    server.use(cURL({
-        log : curl => requests.push(curl)
-    }));
-    server.use(router);
+    server.use(busboy());
+    server.use(middleware);
+    server.post('/answer_with_curl', (req, res) => res.send(req._curl));
+    // server.use('/multiparty', busboy());
+    server.use('/jsonServer', router);
     await new Promise(res => {
         server.listen(port, res);
     });
@@ -40,13 +50,13 @@ test('Get all users', async function () {
     assert.deepEqual(
         await axios({
             method : 'GET',
-            url    : `http://localhost:${port}/users?limit=5`
+            url    : `http://localhost:${port}/jsonServer/users?limit=5`
         }).then(r => r.data),
         users
     );
 
     assert.exists(
-        requests.find(r => r === "curl -X GET -H 'accept: application/json, text/plain, */*' -H 'user-agent: axios/0.21.1' -H 'host: localhost:21356' -H 'connection: close' http://localhost:21356/users?limit=5")
+        requests.find(r => r === "curl -X GET -H 'accept: application/json, text/plain, */*' -H 'user-agent: axios/0.21.1' -H 'host: localhost:21356' -H 'connection: close' http://localhost:21356/jsonServer/users?limit=5")
     );
 });
 
@@ -60,26 +70,116 @@ test('Add user', async function () {
     assert.deepEqual(
         await axios({
             method : 'POST',
-            url    : `http://localhost:${port}/users`,
+            url    : `http://localhost:${port}/jsonServer/users`,
             data   : user
         }).then(r => r.data),
         { ...user, id: 3 }
     );
 
     assert.exists(
-        requests.find(r => r === "curl -X POST -H 'accept: application/json, text/plain, */*' -H 'content-type: application/json;charset=utf-8' -H 'user-agent: axios/0.21.1' -H 'content-length: 44' -H 'host: localhost:21356' -H 'connection: close' -d '{\"name\":\"Ortega\",\"email\":\"lujnipwob@eve.sn\"}' http://localhost:21356/users")
+        requests.find(r => r === "curl -X POST -H 'accept: application/json, text/plain, */*' -H 'content-type: application/json;charset=utf-8' -H 'user-agent: axios/0.21.1' -H 'content-length: 44' -H 'host: localhost:21356' -H 'connection: close' -d '{\"name\":\"Ortega\",\"email\":\"lujnipwob@eve.sn\"}' http://localhost:21356/jsonServer/users")
     );
 });
 
-test('Formdata', async function () {
+test('busboy Formdata', async function () {
     const bodyFormData = new FormData();
 
     bodyFormData.append('name', 'Fred');
-    bodyFormData.append('image', '/tehuker/bejdig.jpeg');
-    await axios({
+    bodyFormData.append('age', 25);
+    bodyFormData.append('images', fs.createReadStream(path.join(__dirname, '../files/liverpool.png')));
+    bodyFormData.append('images', fs.createReadStream(path.join(__dirname, '../files/liverpool.png')));
+    bodyFormData.append('logo', fs.createReadStream(path.join(__dirname, '../files/liverpool.png')));
+    const response = await axios({
         method  : 'POST',
-        url     : `http://localhost:${port}/users`,
+        url     : `http://localhost:${port}/answer_with_curl`,
         data    : bodyFormData,
-        headers : { 'Content-Type': 'multipart/form-data' }
+        headers : bodyFormData.getHeaders()
+    });
+
+    assert.equal(
+        response.data,
+        `curl -X POST -H 'accept: application/json, text/plain, */*' -H 'content-type: multipart/form-data; boundary=${bodyFormData.getBoundary()}' -H 'user-agent: axios/0.21.1' -H 'host: localhost:21356' -H 'connection: close' -H 'transfer-encoding: chunked' -F 'name=Fred' -F 'age=25' -F 'images=@liverpool.png' -F 'images=@liverpool.png' -F 'logo=@liverpool.png' http://localhost:21356/answer_with_curl`
+    );
+});
+
+
+test('Attach to request', async function () {
+    assert.equal(
+        await new Promise(async (resolve, reject) => {
+            server.use('/check_attach', (req, res) => {
+                try {
+                    assert.exists(req._curl);
+                    res.sendStatus(200);
+                    resolve(req._curl);
+                } catch (error) {
+                    reject(error);
+                }
+            });
+            await axios(`http://localhost:${port}/check_attach`);
+        }),
+        'curl -X GET -H \'accept: application/json, text/plain, */*\' -H \'user-agent: axios/0.21.1\' -H \'host: localhost:21356\' -H \'connection: close\' http://localhost:21356/check_attach'
+    );
+});
+
+
+test('Attach without log', async function () {
+    const key = '_CUSTOM_CURL_ATTACH_KEY';
+
+    await new Promise(async (resolve, reject) => {
+        server.use(
+            '/test_path',
+            cURL({ log: null, attach: key }),
+            (req, res) => {
+                try {
+                    assert.exists(req[key]);
+                    res.sendStatus(200);
+                    resolve(1);
+                } catch (error) {
+                    reject(error);
+                }
+            }
+        );
+        await axios(`http://localhost:${port}/test_path`);
     });
 });
+
+
+test('Basic auth', async function () {
+    assert.deepEqual(
+        await axios({
+            method : 'GET',
+            url    : `http://localhost:${port}/jsonServer/users?limit=5`,
+            auth   : { username: 'admin', password: 'password' }
+        }).then(r => r.data),
+        users
+    );
+
+    assert.exists(
+        requests.find(r => r === "curl -X GET -H 'accept: application/json, text/plain, */*' -H 'user-agent: axios/0.21.1' -H 'host: localhost:21356' -H 'authorization: Basic YWRtaW46cGFzc3dvcmQ=' -H 'connection: close' --user admin:password http://localhost:21356/jsonServer/users?limit=5")
+    );
+});
+
+test('data urlencoded', async function () {
+    const params = new URLSearchParams();
+
+    params.append('name', 'Akexorcist');
+    params.append('age', '28');
+    params.append('position', 'Android Developer');
+    params.append('description', 'birthdate=25-12-1989&favourite=coding%20coding%20and%20coding&company=Nextzy%20Technologies&website=http://www.akexorcist.com/');
+    params.append('awesome', true);
+
+    const response = await axios({
+        method  : 'POST',
+        url     : `http://localhost:${port}/answer_with_curl`,
+        data    : params,
+        headers : {
+            'Content-Type' : 'application/x-www-form-urlencoded'
+        }
+    });
+
+    assert.equal(
+        response.data,
+        'curl -X POST -H \'accept: application/json, text/plain, */*\' -H \'content-type: application/x-www-form-urlencoded\' -H \'user-agent: axios/0.21.1\' -H \'content-length: 231\' -H \'host: localhost:21356\' -H \'connection: close\' --data-urlencode \'name=Akexorcist\' --data-urlencode \'age=28\' --data-urlencode \'position=Android Developer\' --data-urlencode \'description=birthdate=25-12-1989&favourite=coding%20coding%20and%20coding&company=Nextzy%20Technologies&website=http://www.akexorcist.com/\' --data-urlencode \'awesome=true\' http://localhost:21356/answer_with_curl'
+    );
+});
+
